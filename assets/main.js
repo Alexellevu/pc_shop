60b70141eed2e1889b52eec082babbe5017e6561
alexellevu <newnapster83@gmail.com>
2021-10-07 19:29:21 +0200

    add new products

--- assets/main.js
+++ assets/main.js
@@ -40,9 +40,13 @@ function addLike(){
 
 
 const products = [
-    new Product ('monitor full hd', 'hight definition screen', '149.00','./assets/img/monitor.jpg', 'pc monitor'),
+    new Product ('monitor full hd', 'hight definition screen', '149.50','./assets/img/monitor.jpg', 'pc monitor'),
     new Product ('monitor hd', 'hight definition screen', '100.00','./assets/img/monitor.jpg', 'pc monitor'),
-    new Product ('monitor full hd', 'hight definition screen', '139.00','./assets/img/monitor.jpg', 'pc monitor'),
+    new Product ('monitor full hd', 'hight definition screen', '139.30','./assets/img/monitor.jpg', 'pc monitor'),
+    new Product ('monitor 4k', '4k screen', '200.00','./assets/img/monitor.jpg', 'pc monitor'),
+    new Product ('monitor full hd', 'hight definition screen', '135.00','./assets/img/monitor.jpg', 'pc monitor'),
+    new Product ('monitor hd', 'hight definition screen', '140.00','./assets/img/monitor.jpg', 'pc monitor'),
+    new Product ('monitor full hd', 'hight definition screen', '139.30','./assets/img/monitor.jpg', 'pc monitor'),
     new Product ('monitor 4k', '4k screen', '200.00','./assets/img/monitor.jpg', 'pc monitor'),
     new Product ('monitor full hd', 'hight definition screen', '135.00','./assets/img/monitor.jpg', 'pc monitor'),
     new Product ('monitor hd', 'hight definition screen', '140.00','./assets/img/monitor.jpg', 'pc monitor')
@@ -54,19 +58,21 @@ const products = [
 /* crea una funzione per stampare i prodotti */
 function printProduct(list, DomEl){
     list.forEach(product => {
-        console.log(product);
+        /* console.log(product); */
         const {name, description, price, image_path, category}  = product;/* destrutturazione oggetto */
         const markup = `
             <div class="product">
-                <img class="img-fluid" src="${image_path}" alt="case">
+                 <a href="./product.html"> <img class="img-fluid" src="${image_path}" alt="case"></a>
+               
                 <h4>${name}</h4>
                 <p>${description}</p>
                 <hr>
                 <span>€ ${price}</span>
                 <p>category: ${category}</p>
                 
-                
-                <button class="buy_now" data-product-name="${name}" data-product-price="${price}">Buy now</button>
+                <button class="buy_now btn btn-dark" data-product-name="${name}" data-product-price="${price}">Buy now</button>
+
+              
             </div>
         `
         /*a riga 69 aggiungo dei data al button per identificare il prodotto al click */
@@ -77,26 +83,50 @@ function printProduct(list, DomEl){
 const productsEl= document.getElementById('products');
 printProduct(products, productsEl);
 
+/* milestone pagina singolo prodotto */
+
+
 
 /* Milestone implementazione carrello */
 
     let cart = [];
+    let total_cart = 0;
 
 /* ciclo all' interno dei bottoni , al clik salvo il data corrispondete al bottone*/
     document.querySelectorAll('.buy_now').forEach(button => {
     button.addEventListener('click', function(){
-        console.log(this);
-        /* console.log(this.getAttribute('data-product-name')); */
+        /* console.log(this); */
+       /*  console.log(this.getAttribute('data-product-price')); */
        const name = this.getAttribute('data-product-name');
-       const price = this.getAttribute('data-product-price');
+       const price =this.getAttribute('data-product-price') ;
+       /* console.log(price );*/
        const products_select = {name, price};
-       console.log(products_select);
+     /* console.log(products_select);  */
        cart.push(products_select);
-       console.log(cart);
+        console.log(cart);
+      
+        total_cart +=parseFloat(products_select.price);  
+       
 
-       document.getElementById('cart_view').insertAdjacentHTML('beforeend',`<div>${products_select.name} ${products_select.price}</div>` )
+       document.querySelector('.cart_view').insertAdjacentHTML('beforeend',`<div>${name} ${price}<i id="cancel" value="1" class="fas fa-trash-alt p-2"></i></div>` );
 
+       document.getElementById('total').innerHTML ='totale: ' + total_cart.toFixed(2)  ;
+        
+
+    
+              
+        
+     
+
+       
+    
     })
   
-
+    
 });
+
+
+                
+                
+
+  
